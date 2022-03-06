e3c8ddb3cb220eb5dc6cd56448f7ba0c9edcb511
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-03-05 19:31:34 -0800

    added files

---
+++ stories.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+
+class Story:
+    def __init__(self, words, text):
+        """Create story with words and template text."""
+
+        self.prompts = words
+        self.template = text
+
+    def generate(self, answers):
+        """Substitute answers into text."""
+
+        text = self.template
+
+        for (key, val) in answers.items():
+            text = text.replace("{" + key + "}", val)
+
+        return text
+
+
+story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
